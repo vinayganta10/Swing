8b6f669fa1fe966acf1ca17425b20fecad431b2b
Vinay ganta <110545176+vinayganta10@users.noreply.github.com>
2023-07-16 19:34:18 +0530

    Add files via upload

---
+++ BorderLayoutExample.java
@@ -0,0 +1,29 @@
+package AbstractWindowTool;
+
+import java.awt.*;
+import javax.swing.*;
+public class BorderLayoutExample
+{
+    JFrame jframe;
+    BorderLayoutExample()
+    {
+        jframe = new JFrame();
+        JButton btn1 = new JButton("NORTH");
+        JButton btn2 = new JButton("SOUTH");
+        JButton btn3 = new JButton("EAST");
+        JButton btn4 = new JButton("WEST");
+        JButton btn5 = new JButton("CENTER");
+        jframe.setLayout(new BorderLayout(20, 15));
+        jframe.add(btn1, BorderLayout.NORTH);
+        jframe.add(btn2, BorderLayout.SOUTH);
+        jframe.add(btn3, BorderLayout.EAST);
+        jframe.add(btn4, BorderLayout.WEST);
+        jframe.add(btn5, BorderLayout.CENTER);
+        jframe.setSize(300,300);
+        jframe.setVisible(true);
+    }
+    public static void main(String argvs[])
+    {
+        new BorderLayoutExample();
+    }
+}
